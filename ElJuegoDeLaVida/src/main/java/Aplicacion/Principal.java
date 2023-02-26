98c5b3985b5e1ff5d413cbce48c2063fe50c1a81
Pablo <113892146+PabloRoher@users.noreply.github.com>
2023-02-26 11:57:25 +0000

    Add files via upload

--- ElJuegoDeLaVida/src/main/java/Aplicacion/Principal.java
+++ ElJuegoDeLaVida/src/main/java/Aplicacion/Principal.java
@@ -1,23 +1,27 @@
 package Aplicacion;
 
-import dominio.Tablero;
+import org.example.Tablero;
+
 import java.util.concurrent.TimeUnit;
-import java.lang.InterruptedException;
-public class Principal{
-    public static void main(String[] args){
+
+public class Principal {
+    public static void main(String[] args) {
         try {
             Tablero tablero = new Tablero();
             System.out.println("SIMULACIÓN CON TABLERO LEÍDO ");
             tablero.leerEstadoActual();
             System.out.println(tablero);
+
             for(int i = 0; i <= 5; i++)
             {
                 TimeUnit.SECONDS.sleep(1);
                 tablero.transitarAlEstadoSiguiente();
                 System.out.println(tablero);
             }
+
             System.out.println("SIMULACIÓN CON TABLERO GENERADO MEDIANTE MONTECARLO");
-                    tablero.generarEstadoActualPorMontecarlo();
+            tablero.generarEstadoActualPorMontecarlo(0.5);
+
             System.out.println(tablero);
             for(int i = 0; i <= 15; i++)
             {
@@ -25,9 +29,12 @@ public class Principal{
                 tablero.transitarAlEstadoSiguiente();
                 System.out.println(tablero);
             }
+
+
         }catch(InterruptedException e)
         {
             System.out.println(e);
         }
+
     }
 }
--- ElJuegoDeLaVida/src/main/java/dominio/Tablero.java
+++ ElJuegoDeLaVida/src/main/java/dominio/Tablero.java
@@ -1,56 +1,101 @@
-package dominio;
+package org.example;
 
-/**
- * Esta clase es responsable de leer el tablero de un
- * fichero en forma de ceros y unos, ir transitando de
- * estados e ir mostrando dichos estados.
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
 public class Tablero {
+
     private static int DIMENSION = 30;
-    private int[][] estadoActual; //matriz que representa el
-    // estado actual.
-    private int[][] estadoSiguiente
-            = new int[DIMENSION][DIMENSION]; // Matriz que
+    private int[][] estadoActual;
+    private int[][] estadoSiguiente = new int[DIMENSION][DIMENSION];;
 
-    // representa el
-// estado
-// siguiente.
+    int fila = 0;
+    int columnas = 0;
+
     public void leerEstadoActual() {
-    }
-// La secuencia de ceros y unos del fichero es guardada
-// en ‘estadoActual‘ y, utilizando las reglas del juego
-// de la vida, se insertan los ceros y unos
-// correspondientes en ‘estadoSiguiente‘.
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/java/dominio/matriz.txt"))) {
+            String linea;
 
-    /********************************************************
-     * Genera un estado inicial aleatorio. Para cada celda
-     * genera un número aleatorio en el intervalo [0, 1). Si
-     * el número es menor que 0,5, entonces la celda está
-     * inicialmente viva. En caso contrario, está muerta.
-     *******************************************************/
-    public void generarEstadoActualPorMontecarlo() {
+            while ((linea = br.readLine()) != null) {
+                if (columnas == 0) {
+                    columnas = linea.length();
+                    estadoActual = new int[columnas][columnas];
+                }
+                for (int columna = 0; columna < columnas; columna++) {
+                    char c = linea.charAt(columna);
+                    estadoActual[fila][columna] = c == '1' ? 1 : 0;
+                }
+                fila++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-// La secuencia de ceros y unos generada es guardada
-// en ‘estadoActual‘ y, utilizando las reglas del juego
-// de la vida, se insertan los ceros y unos
-// correspondientes en ‘estadoSiguiente‘.
 
-    /********************************************************
-     * Transita al estado siguiente según las reglas del
-     * juego de la vida.
-     ********************************************************/
+    private int contarVecinos(int fila, int columna) {
+        int vecinosVivos = 0;
+        for (int i = fila - 1; i <= fila + 1; i++) {
+            for (int j = columna - 1; j <= columna + 1; j++) {
+                if (i == fila && j == columna) {
+                    continue;
+                }
+                int filaValida = (i + DIMENSION) % DIMENSION;
+                int columnaValida = (j + DIMENSION) % DIMENSION;
+                if (estadoActual[filaValida][columnaValida] == 1) {
+                    vecinosVivos++;
+                }
+            }
+        }
+        return vecinosVivos;
+    }
+
     public void transitarAlEstadoSiguiente() {
+        for (int fila = 0; fila < DIMENSION; fila++) {
+            for (int columna = 0; columna < DIMENSION; columna++) {
+                int vecinosVivos = contarVecinos(fila, columna);
+                if (estadoActual[fila][columna] == 1 && (vecinosVivos == 2 || vecinosVivos == 3)) {
+                    estadoSiguiente[fila][columna] = 1;
+                } else if (estadoActual[fila][columna] == 0 && vecinosVivos == 3) {
+                    estadoSiguiente[fila][columna] = 1;
+                } else {
+                    estadoSiguiente[fila][columna] = 0;
+                }
+            }
+        }
+        estadoActual = estadoSiguiente;
+        estadoSiguiente = new int[DIMENSION][DIMENSION];
     }
-// La variable ‘estadoActual‘ pasa a tener el contenido
-// de ‘estadoSiguiente‘ y, éste útimo atributo pasar a
-// reflejar el siguiente estado.
 
-    /*******************************************************
-     * Devuelve, en modo texto, el estado actual.
-     * @return el estado actual.
-     *******************************************************/
+    public void generarEstadoActualPorMontecarlo(double probabilidad) {
+        Random r = new Random();
+        for (int fila = 0; fila < DIMENSION; fila++) {
+            for (int columna = 0; columna < DIMENSION; columna++) {
+                double random = r.nextDouble();
+                if (random < probabilidad) {
+                    estadoActual[fila][columna] = 1;
+                } else {
+                    estadoActual[fila][columna] = 0;
+                }
+            }
+        }
+    }
+
     @Override
     public String toString() {
-        return ""; // Esta línea hay que modificarla.
+        StringBuilder sb = new StringBuilder();
+        for (int fila = 0; fila < DIMENSION; fila++) {
+            for (int columna = 0; columna < DIMENSION; columna++) {
+                if (estadoActual[fila][columna] == 1) {
+                    sb.append("* ");
+                } else {
+                    sb.append(". ");
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
+
 }
